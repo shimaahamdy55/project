cd4bf7e8da07220906bb340158662e13e2e831b3
shimaahamdy55 <shimaahamdy666666@gmail.com>
2024-07-01 02:05:29 +0300

    first one

--- SGEO/src/hooks/useTranslate.ts
+++
@@ -1,9 +0,0 @@
-import locale from "../locale"
-
-const useTranslate = () => {
-  const lang = sessionStorage.getItem("lang") || "en"
-  const res = lang === "ar" && locale.ar
-  return res
-}
-
-export default useTranslate
